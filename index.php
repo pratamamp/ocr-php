8942249c9048c34cc065f3aa4fa2aeb28fc2b0cc
pratamamp <pratamamp@gmail.com>
2018-10-09 11:28:16 +0700

    add git ignore

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 use thiagoalessio\TesseractOCR\TesseractOCR;
-use thiagoalessio\TesseractOCR\FriendlyErrors;
-use thiagoalessio\TesseractOCR\Command;
-use thiagoalessio\TesseractOCR\Option;
 
-echo (new TesseractOCR('text.png'))
+echo (new TesseractOCR('atlet.jpg'))
 	->run();
 ?>
